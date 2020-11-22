8d1de97bea24b6ffe03c78920499973523b5b86e
mburbage <michael@redcircle.biz>
2020-11-21 19:09:53 -0500

    Font Awesome 5 Pro

---
+++ wp-content/themes/hosted-child/functions.php
@@ -0,0 +1,6 @@
+<?php
+
+	wp_register_style('bridge-childstyle-fa-core', get_stylesheet_directory_uri() . '/font-awesome-5-pro/css/fontawesome.min.css');
+	wp_enqueue_style('bridge-childstyle-fa-core');
+	wp_register_style('bridge-childstyle-fal', get_stylesheet_directory_uri() . '/font-awesome-5-pro/css/light.min.css');
+	wp_enqueue_style('bridge-childstyle-fal');
